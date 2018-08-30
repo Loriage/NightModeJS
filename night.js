cd3ea7cc3f41ef07adc9a6bb403cc736e9cffc13
Seikkun <32834445+Seikkun@users.noreply.github.com>
2018-08-30 15:05:45 +0200

    Update night.js

--- night.js
+++ night.js
@@ -7,9 +7,8 @@
 	}
 })(window, document);
 
-
 ;(function (window, document, undefined) {
-
+	
 	'use strict';
 
 	// Feature test
@@ -19,14 +18,13 @@
 	var nav = document.querySelector('#menu-primary');
 	if (!nav) return;
 
-	// Insert the night mode toggle (Check cookies for icon's use)
-	// Change HTML code bellow by your
-	if (localStorage.getItem('gmtNightMode')) {
+	/* Insert the night mode toggle (Check cookies for icon's use)  **
+	** Change HTML code bellow by yours				*/
+	if (localStorage.getItem('gmtNightMode'))
 		nav.innerHTML += '<li id="night-mode"><a class="mdl-navigation__link" href="#"><i id="iconID" class="zmdi zmdi-brightness-5 zmdi-hc-2x"></i></a></li>';
-	} else {
+	else
 	    	nav.innerHTML += '<li id="night-mode"><a class="mdl-navigation__link" href="#"><i id="iconID" class="zmdi zmdi-brightness-2 zmdi-hc-2x"></i></a></li>';
-	}
-
+	
 	// Get our newly insert toggle
 	var nightMode = document.querySelector('#night-mode');
 	if (!nightMode) return;
@@ -35,16 +33,14 @@
 	nightMode.addEventListener('click', function (event) {
 		event.preventDefault();
 		// Change icon if night mode is on
-		if (document.documentElement.classList.toggle('night-mode') === true) {
+		if (document.documentElement.classList.toggle('night-mode') === true)
 			document.getElementById('iconID').className = "zmdi zmdi-brightness-5 zmdi-hc-2x";
-		} else {
+		else
 		    	document.getElementById('iconID').className = "zmdi zmdi-brightness-2 zmdi-hc-2x";
-		}
 		if ( document.documentElement.classList.contains('night-mode') ) {
 			localStorage.setItem('gmtNightMode', true);
 			return;
 		}
 		localStorage.removeItem('gmtNightMode');
 	}, false);
-
 })(window, document);
